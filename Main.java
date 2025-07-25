d8976bff13fd529346cf9ea949977e9fbd9fd928
Patrick Carlos Souza <patrickcarlos_319@hotmail.com>
2025-07-25 20:23:15 -0300

    Aula 23 - Video 41 : refactor: Nessa aula alteramos apenas o código da classe main, onde práticamos os conceitos de array e arrayList.

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 package NivelIntrermediario.Aula14;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,5 +12,31 @@ public class Main {
         Missoes missao2 = new Missoes("Derrotar Zabuza, ", RankDeMissoes.B);
         missao2.exibirDetalhes();
 
+        String[] ninjasArray = new String[4];
+        ninjasArray[0] = "Naruto";
+        ninjasArray[2] = "Sasuke";
+        ninjasArray[1] = "Sakura";
+        System.out.println("Ninjas Array:" + ninjasArray);
+
+
+        List<String> ninjasList = new ArrayList<>();
+        ninjasList.add("Naruto");
+        ninjasList.add("Sasuke");
+        ninjasList.add("Sakura");
+        ninjasList.add("Kakashi");
+
+        System.out.println("Ninjas List:" + ninjasList);
+
+        //removendo um ninja da lista
+        ninjasList.remove("Naruto");
+        System.out.println("Ninjas List após remoção:" + ninjasList);
+
+        //Trocando um ninja na lista
+        ninjasList.set(0, "Hinata");
+        System.out.println("Ninjas List após troca:" + ninjasList);
+
+        //Ver o tamanho da lista
+        ninjasList.size();
+        System.out.println("Tamanho da lista de ninjas: " + ninjasList.size());
     }
 }
