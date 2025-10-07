8a95b70f086b6c624ea82ea910c5f799f3a387a2
PTK <patrickcarlos_319@hotmail.com>
2025-10-06 22:58:14 -0300

    Reajustando os nuemros da aula de acordo com o Jelly. AULA 26 - ARRAY, LIST, STACK: Revisando os tipos de listas em java, suas funcionalidades e como cada um funciona.

--- Main.java
+++ Main.java
@@ -11,15 +11,32 @@ public class Main {
 
         //Array
         //São estaticos e tem referencia de memória (temos que definir o tamanho do array).
+        System.out.println("----------------Arrays----------------");
         String [] ninjasArray = new String[3];
+        ninjasArray[0] = "1";
+        ninjasArray[1] = "2";
+        ninjasArray[2] = "3";
+        System.out.println("Nome do ninja 1 = " + ninjasArray[0]);
+        System.out.println("Nome do ninja 2 = " + ninjasArray[1]);
 
         //Listas
         //São dinamicas e tamanho aumenta ou diminui conforme adicionamos ou removemos elementos.
         //Podemos usar ArrayList, LinkedList, etc.
-        List<String> ninjasList = new ArrayList<>();
+        //Lista é um pouco mais lenta que o array, mas é mais flexivel.
 
-        //Stack
+        System.out.println("----------------Listas----------------");
+        List<String> ninjasList = new ArrayList<>();
+        ninjasList.add("1");
+        ninjasList.add("2");
+        ninjasList.add("3");
+        ninjasList.add("4");
+        System.out.println("Minha Lista: " + ninjasList);
+
+        //Stack (Pilha)
+        //Exigem uma ordem! O ultimo que entra é o primeiro que sai (LIFO - Last In, First Out).
         //O ultimo elemento que entrou é obrigatoriamente o primeiro a sair.
+        //Não deixa retirar qualquer elemento, só o ultimo que entrou.
+        System.out.println("----------------Stack----------------");
         Stack<String> ninjasStack = new Stack<>();
         ninjasStack.push("Naruto");
         ninjasStack.push("Sasuke");
